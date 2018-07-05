f7d2100a63a42f42ffb50d064f06796561222ae3
aleksandar <alpe88@users.noreply.github.com>
2018-07-05 16:30:48 -0700

    Created components for Books and Bookshelves

---
+++ src/Bookshelf.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Bookshelf extends Component {
+	render() {
+    	return (
+        	<div>
+          		<p>Books will go here.</p>
+          	</div>
+        )
+    }
+}
+
+export default Bookshelf
