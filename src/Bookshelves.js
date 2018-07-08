a62fcf72e11667a4be8a62b0115b0a099ba56773
aleksandar <alpe88@users.noreply.github.com>
2018-07-07 18:45:23 -0700

    added default bookshelf types

--- src/Bookshelves.js
+++ src/Bookshelves.js
@@ -1,90 +1,47 @@
 import React, { Component } from 'react'
-import Shelfchanger from './Shelfchanger'
+import Bookshelf from './Bookshelf'
 
 class Bookshelves extends Component {
   
     state = {
-    }
-    
-	render(){
-      
+      bookshelfs: [
+        {
+         'id':'458485dfbxgc',
+         'type':'wantToRead',
+         'title':'Currently Reading'
+        },
+        {
+         'id':'zcvbm07845',
+         'type':'currentlyReading',
+         'title':'Want to Read'
+        },
+        {
+         'id':'4w6iet6yilu',
+         'type':'read',
+         'title':'Read'
+        },
+        {
+         'id':'08pt7e68576',
+         'type':'none',
+         'title':'None'
+        }
+      ]
+     }
+
+render(){
+      const { bookshelfs } = this.state
       const { books } = this.props
-      console.log(books);
 
-    	return (
-        	<div>
-              <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                         	{books.map((book) => (
-                                <li key={book.id}>
-                                 {book.shelf === 'currentlyReading' && (
-                                  <div className="book">
-                                    <div className="book-top">
-                                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                                      <Shelfchanger bookshelfType={book.shelf} />
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors}</div>
-                                  </div>
-                                  )}
-                                </li>                
-                           ))}
-                    	</ol>
-                  	</div>
-                </div>
-
-
-
-             <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                         	{books.map((book) => (
-                                <li key={book.id}>
-                                 {book.shelf === 'wantToRead' && (
-                                  <div className="book">
-                                    <div className="book-top">
-                                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                                      <Shelfchanger bookshelfType={book.shelf} />
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors}</div>
-                                  </div>
-                                  )}
-                                </li>                
-                           ))}
-                    	</ol>
-                  	</div>
-                </div>
-
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                         	{books.map((book) => (
-                                <li key={book.id}>
-                                 {book.shelf === 'read' && (
-                                  <div className="book">
-                                    <div className="book-top">
-                                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                                      <Shelfchanger bookshelfType={book.shelf} />
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors}</div>
-                                  </div>
-                                  )}
-                                </li>                
-                           ))}
-                    	</ol>
-                  	</div>
-                </div>
-
-          	</div>
-        )
-    }
+      
+      return (
+       <div>
+         {bookshelfs.map((bookshelf) => (
+          <div key={bookshelf.id}>
+              <Bookshelf bookshelf={bookshelf} books={books} />
+          </div>
+         ))}
+        </div>
+      )}
 }
 
 export default Bookshelves
