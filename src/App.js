dc0b7e977631f44ee584a4b191f2c5c141043fd5
aleksandar <alpe88@users.noreply.github.com>
2018-07-08 11:18:09 -0700

    added Router, replacing state based navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import Bookshelf from './Bookshelf'
+import { Route, Link } from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-     
-    showSearchPage: false,
+    
     
       /**
       * Added array to store all books
@@ -61,11 +55,36 @@ class BooksApp extends React.Component {
     
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div>
-       <div className="search-books">
+            <Route exact path='/' render={() => (
+                <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                 <div>
+                    {bookshelfs.map((bookshelf) => (
+                     <div key={bookshelf.id}> 
+                       <Bookshelf bookshelf={bookshelf} books={books} />
+                     </div>
+                    ))}
+          </div>
+        </div>
+              <div className="open-search">
+                <Link
+                  to='/search'
+                >Add a Book</Link>
+              </div>
+            </div>
+          )} />
+
+
+        <Route path='/search' render={({ history }) => (
+          <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <Link
+                to='/'
+                className='close-search'
+              >Close</Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -82,27 +101,10 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-       	</div>
-        ) : (
-		<div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-               <div>
-                  {bookshelfs.map((bookshelf) => (
-                   <div key={bookshelf.id}> 
-                   <Bookshelf bookshelf={bookshelf} books={books} />
-            </div>
-         ))}
-        </div>
-      </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
+          
+        )} />
 
-        )}
+
       </div>
     )
   }
