baae42d46519c9ed932608c7477d7fa91e336b12
aleksandar <alpe88@users.noreply.github.com>
2018-07-10 11:05:31 -0700

    Updated based on advice from Udacity reviewer

--- src/Shelfchanger.js
+++ src/Shelfchanger.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react'
-import * as BooksAPI from './BooksAPI'
 
 class Shelfchanger extends Component {
-
-     updateBookshelfTypeSelector = (newBookshelf) => {
-           this.setState(() => ({
-            currentBookshelf: newBookshelf,
-            })
-          )
-        }
        
-       updateBook = (book, newBookshelf) => {
-        console.log(book.title +' is now on '+ newBookshelf)
-          BooksAPI.update(book,newBookshelf)
-             .then((book) => {
-             this.setState(() => ({
-              currentBookshelf: newBookshelf,
-              })
-            )
-        })
-      }
+       
     
 
 render(){
-     const { book } = this.props
+     const { book, switchBookshelf } = this.props
 
     	return (
         	<div key={book.id}>
           		<div className="book-shelf-changer">
-					<select value={book.shelf} onChange={(event) => this.updateBook(book, event.target.value)}>
+					<select value={book.shelf} onChange={(event) => switchBookshelf(book, event.target.value)}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
