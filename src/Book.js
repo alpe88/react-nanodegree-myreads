480d0edf7af9a9cfd7152f1ea707f21f9402daa5
aleksandar <alpe88@users.noreply.github.com>
2018-07-12 21:44:12 -0700

    more updates, book now moves when switched
    todo: have book show up after being switched.

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react'
 import Shelfchanger from './Shelfchanger'
-import * as BooksAPI from './BooksAPI'
 
 class Book extends Component {
-     state = {
-          currentBookshelf: 'none'
-     }  
 
-updateBook = (book, newBookshelf) => {
-        console.log(book.title +' is now on '+ newBookshelf)
-          BooksAPI.update(book,newBookshelf)
-             .then((book) => {
-              this.setState(() => ({
-                currentBookshelf: newBookshelf
-            })
-          )
-        })
-      }
-  
-
-render(){
-  const { updateBook } = this.state
-   const { book } = this.props
+render(){ 
+   const { book, booksOnShelf, updateBook } = this.props
     return (
       <div>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-              <Shelfchanger book={book} switchBookshelf={updateBook} />
+              <Shelfchanger book={book}  updateBook={updateBook} booksOnShelf={booksOnShelf}/>
             </div>
            <div className="book-title">{book.title}</div>
           <div className="book-authors">{book.authors}</div>
