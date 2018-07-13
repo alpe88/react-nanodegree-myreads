379bd35c984d2300bcff71cd68bbe293f07d88a2
aleksandar <alpe88@users.noreply.github.com>
2018-07-12 21:46:03 -0700

    more updates, book now moves when switched
    todo: have book show up after being switched.

--- src/Shelfchanger.js
+++ src/Shelfchanger.js
@@ -6,12 +6,22 @@ class Shelfchanger extends Component {
     
 
 render(){
-     const { book, switchBookshelf } = this.props
-
+     const { book, updateBook, booksOnShelf } = this.props
+     
+     let currentShelf = 'none'
+     
+     for (let b of booksOnShelf ) {
+      if (b.id === book.id)  {
+        currentShelf = b.shelf
+        console.log('new shelf', currentShelf)
+        break
+      }
+    }
+  
     	return (
         	<div key={book.id}>
-          		<div className="book-shelf-changer">
-					<select value={book.shelf} onChange={(event) => switchBookshelf(book, event.target.value)}>
+                   <div className="book-shelf-changer">
+                     <select defaultValue={ currentShelf } onChange={(event) => updateBook(book, event.target.value)}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
