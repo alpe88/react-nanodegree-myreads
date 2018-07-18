4cdc166a88044c329c1ed944a6adaf824e7fac0d
aleksandar <alpe88@users.noreply.github.com>
2018-07-18 13:33:57 -0700

    Book now switches shelves based on shelf change
    Will need a refactor

--- src/Book.js
+++ src/Book.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react'
 import Shelfchanger from './Shelfchanger'
 
 class Book extends Component {
+  
+  
 
 render(){ 
+   
    const { book, booksOnShelf, updateBook } = this.props
+   const coverImage = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : "No cover available"
+   const bookTitle = book.title ? book.title : "No title available"
+   
     return (
       <div>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${coverImage})` }}></div>
               <Shelfchanger book={book}  updateBook={updateBook} booksOnShelf={booksOnShelf}/>
             </div>
-           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+           <div className="book-title">{bookTitle}</div>
+          { /* Check for authors and render each on separate line if exist*/
+                book.authors && book.authors.map((author, index) => (
+                  <div className="book-authors" key={index}>{author}</div>
+              ))}
         </div>
        </div>
         )
